351441d3741c9895e73b9473c82d10a086708051
Chao Liu <chao.liu@yeah.net>
2024-12-29 20:35:39 +0800

    chore: finish exercise 13

--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -14,11 +14,15 @@ class Fibonacci {
 
 public:
     // TODO: 实现构造器
-    // Fibonacci()
+    Fibonacci() {
+        cache[0] = 0;
+        cache[1] = 1;
+        cache[2] = 1;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (cached = 3; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
