484ff76663754deabc3aded823116c662f135205
Chao Liu <chao.liu@yeah.net>
2025-01-02 23:18:24 +0800

    learning-cxx: finish exercise 32

--- exercises/32_std_transform/main.cpp
+++ exercises/32_std_transform/main.cpp
@@ -2,13 +2,20 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <sstream>  // 包含 std::ostringstream
 
 // READ: `std::transform` <https://zh.cppreference.com/w/cpp/algorithm/transform>
 // READ: `std::vector::begin` <https://zh.cppreference.com/w/cpp/container/vector/begin>
-
+auto transform_func = [](int x) -> std::string {
+    std::ostringstream oss;
+    oss << (x * 2);// 乘以 2 并转换为字符串
+    return oss.str();
+};
 int main(int argc, char **argv) {
     std::vector<int> val{8, 13, 21, 34, 55};
     // TODO: 调用 `std::transform`，将 `v` 中的每个元素乘以 2，并转换为字符串，存入 `ans`
+    std::vector<std::string> ans(val.size());
+    std::transform(val.begin(), val.end(), ans.begin(), transform_func);
     // std::vector<std::string> ans
     ASSERT(ans.size() == val.size(), "ans size should be equal to val size");
     ASSERT(ans[0] == "16", "ans[0] should be 16");
